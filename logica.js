6bb0ce03a6fd7aa47c79e75f5abef47723fc08e5
Professor Jose de Assis <josedeassisfilho@gmail.com>
2024-04-09 21:56:54 -0300

    script para identificar o ano automaticamente

--- logica.js
+++ logica.js
@@ -57,3 +57,8 @@ function sw(receive) {
     }
   }
 }
+
+// Atualizar automaticamente o ano (footer - copyright)  
+let ano = document.getElementById("copyrightYear")
+let anoAtual = new Date().getFullYear()
+ano.textContent = anoAtual
